2537058c552291139974d54690d43ab4bd465d70
jihadkandil2 <jihadkandil2@gmail.com>
2025-03-08 00:51:29 +0200

    files updated

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,25 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config(); // Correctly load .env variables
+import bootstrap from './src/app.controller.js'
+import express from 'express'
 
-const app = express();
-app.use(bodyParser.json());
+const app=express();
+const port =3000;
 
-// Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+bootstrap(app , express)
+
+app.listen(port , ()=>{console.log(`listening on ${port}`);
 })
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
-// Define routes, models, etc.
-app.get('/', (req, res) => {
-  res.send("Hello, World!");
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});

52ba78866e275cd6fd415bff619a9998a24794ad
Hana20-Mohsen <hana2021mohsen@gmail.com>
2025-03-17 01:53:54 +0200

    edit updateProfile method

--- Project/Backend/src/modules/user/services/updateProfile.service.js
+++ Project/Backend/src/modules/user/services/updateProfile.service.js
@@ -1,22 +1,35 @@
 
 import Users from '../../../DB/model/User.model.js'
- const updateProfile= async(req,res,next)=>{
-    try {
+import bcrypt from 'bcrypt'
 
+const updateProfile = async (req, res, next) => {
+    try {
+        const { oldPassword , newPassword } = req.body;
+
+        if (oldPassword) {
+            const user = await Users.findById(req.params.id)
+            if (!user) return res.status(404).json({ error: "User not found" });
+            const match = bcrypt.compareSync(oldPassword, user.password)
+            if (!match) {
+                return res.status(404).json({ message: 'incorrect password' })
+            }
+            user.password=bcrypt.hashSync(newPassword , parseInt(process.env.SALT))
+            await user.save();
+        }
         const updatedUser = await Users.findByIdAndUpdate(
             req.params.id,
             req.body,
-            { new: true, runValidators: true } 
+            { new: true, runValidators: true }
         );
         if (!updatedUser) {
             return res.status(404).json({ message: 'user not found' });
         }
         return res.status(200).json({
-            message:'user  data updated successfully',
+            message: 'user  data updated successfully',
             updatedUser
         })
     } catch (error) {
-        return res.status(500).json({message:'error' , error})
+        return res.status(500).json({ message: 'error', error })
     }
 }
 

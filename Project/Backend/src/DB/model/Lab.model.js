2eaf99ffe81e66e67e225e293ae6f4a4ea54b47b
Hana20-Mohsen <hana2021mohsen@gmail.com>
2025-05-10 17:43:15 +0300

    new data added to database

--- Project/Backend/src/DB/model/Lab.model.js
+++ Project/Backend/src/DB/model/Lab.model.js
@@ -2,11 +2,13 @@ import mongoose, {Schema , model} from 'mongoose';
 
  const labSchema = new Schema(
     {
-        labScenario: { type: String, required: true },
+    labScenario: { type: String, required: true },
     labDescription: { type: String, required: true },//what about this?
     vulnerabilityName: { type: Schema.Types.Mixed, ref: 'Vulnerability' , required:true },
+    SolutionSteps: { type: [String], required: true },
     labLevel: { type: String, required: true },
-    srcCode: String
+    srcCode: { type: String },
+    payloads: { type: [String] }
     }, { timestamps: true }
 )
  const Lab= model('Lab' , labSchema)|| mongoose.models.Lab  ;
